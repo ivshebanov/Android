11da0dbdc5b2455ee27cf1a451161f958ebe38d7
Ilya Shebanov <ivshebanov@gmail.com>
2017-07-17 23:04:56 +0300

    ReaderFile

---
+++ ReaderFile/app/src/main/java/com/example/android/readerfile/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.android.readerfile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    //proc/sys/net/ipv4/ip_default_ttl
+
+//    private final static String FILE_NAME = "content.txt";
+    private final static String FILE_NAME = "/proc/sys/net/ipv4/content.txt";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // сохранение файла
+    public void saveText(View view) {
+
+        FileOutputStream fos = null;
+        try {
+            EditText textBox = (EditText) findViewById(R.id.save_text);
+            String text = textBox.getText().toString();
+
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            Toast.makeText(this, "Файл сохранен", Toast.LENGTH_SHORT).show();
+        } catch (IOException ex) {
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        } finally {
+            try {
+                if (fos != null)
+                    fos.close();
+            } catch (IOException ex) {
+
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // открытие файла
+    public void openText(View view) {
+
+        FileInputStream fin = null;
+        TextView textView = (TextView) findViewById(R.id.open_text);
+        try {
+            fin = openFileInput(FILE_NAME);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            textView.setText(text);
+        } catch (IOException ex) {
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        } finally {
+
+            try {
+                if (fin != null)
+                    fin.close();
+            } catch (IOException ex) {
+
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
